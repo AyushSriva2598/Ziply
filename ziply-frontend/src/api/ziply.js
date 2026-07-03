6f9cb6d17b134a3359587b87d487400d767ddd0e
AyushSriva2598 <ayushsriva2598@example.com>
2026-07-03 22:13:08 +0530

    Vercel raliway api connection ziply

--- ziply-frontend/src/api/ziply.js
+++ ziply-frontend/src/api/ziply.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE;
 
 export async function shortenUrl(longUrl) {
   const res = await fetch(`${BASE_URL}/api/v1/shorten/`, {
